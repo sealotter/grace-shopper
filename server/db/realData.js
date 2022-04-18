98d252d9b89d4c9612aa3b844f5b42479968c9ea
Lisa Knox <lisaknox83@gmail.com>
2022-04-18 09:24:23 -0400

    albums show when logged out

--- server/db/realData.js
+++ server/db/realData.js
@@ -31,20 +31,41 @@ const sampleData = async (release) => {
   }
 };
 
-const searchTest = async (artist, title) => {
+const getAlbumDetails = async (artist, title) => {
   try {
     const searchResults = await axios.get(
       `https://api.discogs.com/database/search?q=artist=${artist}&title=${title}&key=${process.env.DISCOGS_KEY}&secret=${process.env.DISCOGS_SECRET}`
     );
-    const output = searchResults.data.results[0].resource_url;
-    console.log(typeof output);
+    const response = (
+      await axios.get(searchResults.data.results[0].resource_url)
+    ).data;
+    // console.log(response);
+    const album = {
+      //format included for sorting purposes
+      format: response.formats[0].name,
+      albumName: response.title,
+      albumArt: searchResults.data.results[0].cover_image,
+      artistName: response.artists[0].name,
+      genre: response.genres[0],
+      style: response.styles[0],
+      year: response.year,
+      price: response.lowest_price,
+      albumDetails: response.notes,
+      trackList: response.tracklist.map((x) => {
+        return { track: x.position, title: x.title };
+      }),
+      rating: response.community.rating.average,
+      availableInventory: response.num_for_sale,
+    };
+    console.log('--within trycatch--', album);
+    return album;
   } catch (error) {
     console.log(error);
   }
 };
 
-searchTest('The Clash', 'London Calling');
+console.log(getAlbumDetails('The Clash', 'London Calling'));
 
-const realData = sampleData();
+const realData = getAlbumDetails('The Clash', 'London Calling');
 
 module.exports = realData;
