42862a67259589d120206d1e683309c6c9ef439b
Lisa Knox <lisaknox83@gmail.com>
2022-04-17 21:46:03 -0400

    basic search functionality is up

--- server/db/realData.js
+++ server/db/realData.js
@@ -2,11 +2,12 @@ const app = require('../app');
 const axios = require('axios');
 const res = require('express/lib/response');
 
-const sampleData = async () => {
+const sampleData = async (release) => {
   try {
     const response = (
-      await axios.get('https://api.discogs.com/releases/22628222')
+      await axios.get(`https://api.discogs.com/releases/8928025`)
     ).data;
+    // console.log(response);
     const album = {
       //format included for sorting purposes
       format: response.formats[0].name,
@@ -14,39 +15,35 @@ const sampleData = async () => {
       albumArt: 'authorization required',
       artistName: response.artists[0].name,
       genre: response.genres[0],
+      style: response.styles[0],
       year: response.year,
       price: response.lowest_price,
-      responseDetails: response.notes,
+      albumDetails: response.notes,
       trackList: response.tracklist.map((x) => {
         return { track: x.position, title: x.title };
       }),
       rating: response.community.rating.average,
       availableInventory: response.num_for_sale,
     };
-    // console.log(album);
+    console.log(album);
   } catch (error) {
-    console.log(error);
+    console.log(error.code);
   }
 };
 
-const searchParam = 'Punk';
-
-const searchTest = async () => {
+const searchTest = async (artist, title) => {
   try {
     const searchResults = await axios.get(
-      `https://api.discogs.com/database/search?q=${searchParam}&key=${process.env.DISCOGS_KEY}&secret=${process.env.DISCOGS_SECRET}`
+      `https://api.discogs.com/database/search?q=artist=${artist}&title=${title}&key=${process.env.DISCOGS_KEY}&secret=${process.env.DISCOGS_SECRET}`
     );
-    for (let key in searchResults.data) {
-      console.log(searchResults.data[key]);
-    }
-    // console.log(searchResults.data);
+    const output = searchResults.data.results[0].resource_url;
+    console.log(typeof output);
   } catch (error) {
     console.log(error);
   }
 };
 
-console.log(searchTest());
-//
+searchTest('The Clash', 'London Calling');
 
 const realData = sampleData();
 
