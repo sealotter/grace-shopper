0eb35b189dbd2d7919e170a6bc268b36d0ea235d
Lisa Knox <lisaknox83@gmail.com>
2022-04-27 20:37:13 -0400

    removed one unused import

--- client/components/AlbumList.js
+++ client/components/AlbumList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AlbumDetail from './AlbumDetail';
 
 const AlbumList = (props) => {
